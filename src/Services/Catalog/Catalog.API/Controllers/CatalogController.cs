3b04fdf8fe45d952ab3261fb6b05e58d4ea65813
balogunpraise <balogunpraise2@gmail.com>
2023-07-20 23:01:01 +0100

    initial project setup with classes

---
+++ src/Services/Catalog/Catalog.API/Controllers/CatalogController.cs
@@ -0,0 +1,66 @@
+﻿using Catalog.API.Entitites;
+using Catalog.API.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Catalog.API.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class CatalogController : ControllerBase
+    {
+        private readonly IProductRepository _repo;
+        private readonly ILogger<CatalogController> _logger;
+        public CatalogController(IProductRepository repo, ILogger<CatalogController> logger)
+        {
+            _repo = repo ?? throw new ArgumentException(nameof(repo));
+            _logger = logger ?? throw new ArgumentException(nameof(logger));
+        }
+
+        #region Query
+
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<Product>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<IEnumerable<Product>>> Products()
+        {
+            var products = await _repo.GetProducts();
+            return Ok(products);
+        }
+
+        [HttpGet("{id:length(24)}", Name = "GetProduct")]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        [ProducesResponseType(typeof(Product), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<Product>> GetProductById(string id)
+        {
+            var product = await _repo.GetProductById(id);
+            if(product is null)
+            {
+                _logger.LogError($"Product with id: {id}, not found");
+                return NotFound();
+            }
+            return Ok(product);
+        }
+
+        [HttpGet]
+        [Route("[action]/{category}", Name = "GetProductByCategory")]
+        [ProducesResponseType(typeof(Product), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<Product>> GetProductByCategory(string category)
+        {
+            var products = await _repo.GetProductByCategory(category);
+            return Ok(products);
+        }
+
+        #endregion
+
+        #region Command
+        [HttpPost]
+        [ProducesResponseType(typeof(Product), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<Product>> Product([FromBody] Product product)
+        {
+            await _repo.CreateProduct(product);
+            return CreatedAtRoute("GetProdunct", new {id = product.Id}, product);
+        }
+        #endregion
+    }
+}
