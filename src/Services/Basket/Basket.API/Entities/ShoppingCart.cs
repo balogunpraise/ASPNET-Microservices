98cebedfdc593a3ad0104ad51bf41dd983ea97b1
balogunpraise <balogunpraise2@gmail.com>
2023-07-22 00:13:26 +0100

    Added the basket api microservice

---
+++ src/Services/Basket/Basket.API/Entities/ShoppingCart.cs
@@ -0,0 +1,26 @@
+﻿namespace Basket.API.Entities
+{
+    public class ShoppingCart
+    {
+        public string UserName { get; set; }
+        public List<ShoppingCartItem> Items { get; set; } = new List<ShoppingCartItem>();
+
+        public ShoppingCart()
+        {
+            
+        }
+        public ShoppingCart(string userNamer)
+        {
+            UserName = userNamer;
+        }
+
+        public decimal TotalPrice 
+        {
+            get
+            {
+                decimal totalPrice = 0;
+                return totalPrice = (decimal)Items?.Select(x => (x.Price * x.Quantity)).Sum();
+            }
+        }
+    }
+}
