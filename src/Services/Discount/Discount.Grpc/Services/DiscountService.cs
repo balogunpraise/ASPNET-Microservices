96acc3b1824198533e5fbaa8bc2ed8caaca4fc9a
balogunpraise <balogunpraise2@gmail.com>
2023-07-25 21:21:58 +0100

    added a grpc project

---
+++ src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
@@ -0,0 +1,29 @@
+﻿using Discount.Grpc.Protos;
+using Discount.Grpc.Repositories;
+using Grpc.Core;
+
+namespace Discount.Grpc.Services
+{
+    public class DiscountService : DiscountProtoService.DiscountProtoServiceBase
+    {
+        private readonly IDiscountRepository _repo;
+        private readonly ILogger<DiscountService> _logger;
+
+        public DiscountService(IDiscountRepository repo, ILogger<DiscountService> logger)
+        {
+            _repo = repo ?? throw new ArgumentException(nameof(repo));
+            _logger = logger ?? throw new ArgumentException(nameof(repo));
+        }
+
+        public override async Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+        {
+            var coupon = await _repo.GetDiscount(request.ProductName);
+            if(coupon is null)
+            {
+                throw new RpcException(new Status(StatusCode.NotFound, $"Discount with ProductName={request.ProductName}"));
+            }
+            var couponModel = _mapper.Map<CouponModel>(coupon);
+            return couponModel;
+        }
+    }
+}
